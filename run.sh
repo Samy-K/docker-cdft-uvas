982e8b5de221b8b4bb5c669abdbe5040a210515b
Samy K <58725087+Samy-K@users.noreply.github.com>
2020-01-30 15:43:49 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,6 @@ hdfscli download --alias=dev -f /user/Samy-K/data_model /data/code/
 hdfscli download --alias=dev -f /user/Samy-K/data_station /data/code/
 hdfscli download --alias=dev -f /user/Samy-K/le_correcteur /data/code/
 
-
 Rscript le_correcteur/le_correcteur.R
-
 
 hdfscli upload --alias=dev -f /data/code/out /user/Samy-K/ 
