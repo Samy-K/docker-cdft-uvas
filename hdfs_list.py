5b119fa3aba552d3b0ab957c6f74fe5433947afa
Samy K <58725087+Samy-K@users.noreply.github.com>
2020-01-30 15:42:58 +0100

    Update hdfs_list.py

--- hdfs_list.py
+++ hdfs_list.py
@@ -2,7 +2,7 @@ from hdfs import Config
 
 client = Config().get_client('dev')
 
-fnames = client.list('/user/Samy-K/')
-print(fnames)
+files_names = client.list('/user/Samy-K/')
+print(files_names)
 
 
